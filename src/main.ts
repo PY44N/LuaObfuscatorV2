69339903170a88c63057ba526cb679719b58b9d5
PY44N <pyan4444@gmail.com>
2022-10-04 19:47:02 -0400

    Did int conversions

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,17 @@ import {
 import { execSync } from "child_process";
 import { Deserializer } from "./bytecode/Deserializer";
 
-if (existsSync("temp")) rmdirSync("temp", { recursive: true });
-mkdirSync("temp");
+// if (existsSync("temp")) rmdirSync("temp", { recursive: true });
+// mkdirSync("temp");
 
-copyFileSync("Input.lua", "./temp/temp1.lua");
+// copyFileSync("Input.lua", "./temp/temp1.lua");
 
-execSync("luac temp1.lua", { cwd: "temp" });
+// execSync("luac temp1.lua", { cwd: "temp" });
 
-let compiled = readFileSync("./temp/luac.out", "utf8")
+let compiled = readFileSync("./temp/luac.out", "binary")
   .split("")
   .map((v) => v.charCodeAt(0));
+
+console.log(compiled);
 
 let deserializer = new Deserializer(compiled);
--- src/util/MemoryStream.ts
+++ src/util/MemoryStream.ts
@@ -1,7 +1,32 @@
 export class MemoryStream {
   bytes: number[];
+  position: number;
 
   constructor(bytes: number[]) {
     this.bytes = bytes;
+    this.position = 0;
+  }
+
+  Read(count: number): number[] {
+    //TODO: Endianess Things
+    return this.bytes.slice(this.position, (this.position += count));
+  }
+
+  ReadInt8(): number {
+    return this.Read(1)[0];
+  }
+
+  ReadInt16(): number {
+    let [a, b] = this.Read(2);
+    return a + b * 2 ** 8;
+  }
+
+  ReadInt32(): number {
+    let [a, b, c, d] = this.Read(4);
+    return a + b * 2 ** 8 + c * 2 ** 16 + d * 2 ** 24;
+  }
+
+  ReadInt64(): number {
+    return this.ReadInt32() + this.ReadInt32() * 2 ** 32;
   }
 }
