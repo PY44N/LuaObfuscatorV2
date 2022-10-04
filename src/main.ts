6ab8056fdac999ef8b21ce7f1cfaaddccaf2c7d7
PY44N <pyan4444@gmail.com>
2022-10-04 09:56:27 -0400

    Got it to compile

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("Hello")
+import {
+  readFileSync,
+  mkdirSync,
+  copyFileSync,
+  existsSync,
+  rmdirSync,
+} from "fs";
+import { execSync } from "child_process";
+
+if (existsSync("temp")) rmdirSync("temp", { recursive: true });
+mkdirSync("temp");
+
+copyFileSync("Input.lua", "./temp/temp1.lua");
+
+execSync("luac temp1.lua", { cwd: "temp" });
