35091220b46f8f0df7ed87723b818a1b92fb61ac
PY44N <pyan4444@gmail.com>
2022-10-13 09:54:29 -0400

    Deserialized the header

--- src/bytecode/Deserializer.ts
+++ src/bytecode/Deserializer.ts
@@ -26,6 +26,35 @@ export class Deserializer {
   }
 
   deserialize() {
-    console.log(this.readString());
+    if (this.readString(4) != String.fromCharCode(27) + "Lua")
+      throw new Error("Invalid file header");
+
+    if (this.byteStream.readInt8() != 0x51)
+      throw new Error("Invalid lua version, only Lua 5.1 supported");
+
+    if (this.byteStream.readInt8() != 0)
+      throw new Error("Invalid format versions");
+
+    //TODO: Big Endian Support
+    //Endianess value (1 = little 0 = big)
+    if (this.byteStream.readInt8() != 1) throw new Error("Invalid endianess");
+
+    this.intSize = this.byteStream.readInt8();
+    if (this.intSize != 4 && this.intSize != 8)
+      throw new Error("Invalid int size");
+
+    this.sizeTSize = this.byteStream.readInt8();
+    if (this.sizeTSize != 4 && this.sizeTSize != 8)
+      throw new Error("Invalid SizeT size");
+
+    if (this.byteStream.readInt8() != 4)
+      throw new Error("Invalid Instruction Size");
+
+    //TODO: Add float (4-bit) number support
+    if (this.byteStream.readInt8() != 8)
+      throw new Error("Invalid lua number size");
+
+    if (this.byteStream.readInt8() != 0)
+      throw new Error("Invalid integral flag");
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ import {
 import { execSync } from "child_process";
 import { Deserializer } from "./bytecode/Deserializer";
 
-// if (existsSync("temp")) rmdirSync("temp", { recursive: true });
-// mkdirSync("temp");
+if (existsSync("temp")) rmdirSync("temp", { recursive: true });
+mkdirSync("temp");
 
-// copyFileSync("Input.lua", "./temp/temp1.lua");
+copyFileSync("Input.lua", "./temp/temp1.lua");
 
-// //TODO: Maybe strip comments and minify before compiling
-// execSync("luac temp1.lua", { cwd: "temp" });
+//TODO: Maybe strip comments and minify before compiling
+execSync("luac temp1.lua", { cwd: "temp" });
 
 let compiled = readFileSync("./temp/luac.out", "binary")
   .split("")
