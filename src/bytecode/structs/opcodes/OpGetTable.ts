3830e5ff33e354f8ce1073ab61498b83d522a364
PY44N <pyan4444@gmail.com>
2022-11-01 11:46:47 -0400

    Pain, Jimmy Eat World

--- src/bytecode/structs/opcodes/OpGetTable.ts
+++ src/bytecode/structs/opcodes/OpGetTable.ts
@@ -1,6 +1,15 @@
+import { Instruction } from "../Instruction";
 import { Opcode } from "../Opcode";
 
 export class OpGetTable extends Opcode {
+  instruction: Instruction;
+
+  constructor(instruction: Instruction) {
+    super();
+
+    this.instruction = instruction;
+  }
+
   getObfuscated(): string {
     throw new Error("Method not implemented.");
   }
