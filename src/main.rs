56d39fb1b0aeeab1bc831a6f098211fba671a5d7
PY44N <pyan4444@gmail.com>
2022-11-07 09:54:14 -0500

    Compiled and read bytes from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
-use std::fs;
+use std::{
+    fs::{self, File},
+    io::{BufReader, Read},
+    path::Path,
+    process::Command,
+};
 
 fn main() {
-    
+    if Path::new("temp").is_dir() {
+        fs::remove_dir_all("temp").unwrap();
+    }
+    fs::create_dir("temp").unwrap();
+
+    fs::copy("Input.lua", "temp/temp1.lua").unwrap();
+
+    let luac_command = if cfg!(target_os = "windows") {
+        "luac"
+    } else {
+        "luac5.1"
+    };
+
+    Command::new(luac_command)
+        .arg("temp1.lua")
+        .current_dir("temp")
+        .output()
+        .expect("Failed to compile lua binary");
+
+    let mut reader = BufReader::new(File::open("temp/luac.out").unwrap());
+    let mut buffer = Vec::new();
+
+    reader.read_to_end(&mut buffer).unwrap();
 }
