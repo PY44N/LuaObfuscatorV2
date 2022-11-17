b60fae91a932e86756baf0a3aba947c0a7717e31
PY44N <pyan4444@gmail.com>
2022-11-17 09:57:19 -0500

    Started the deserializer

--- src/bytecode/deserializer.rs
+++ src/bytecode/deserializer.rs
@@ -1,3 +1,17 @@
-pub struct Deserializer {}
+use crate::MemoryStream;
 
-impl Deserializer {}
+pub struct Deserializer {
+    memory_stream: MemoryStream,
+}
+
+impl Deserializer {
+    pub fn new(bytes: Vec<u8>) -> Self {
+        Self {
+            memory_stream: MemoryStream::new(bytes),
+        }
+    }
+
+    pub fn deserialize(&mut self) {
+        println!("{:?}", self.memory_stream.read_string(4));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,36 +7,36 @@ use std::{
 
 use util::memory_stream::MemoryStream;
 
+use crate::bytecode::deserializer::Deserializer;
+
+pub mod bytecode;
 pub mod util;
 
 fn main() {
-    // if Path::new("temp").is_dir() {
-    //     fs::remove_dir_all("temp").unwrap();
-    // }
-    // fs::create_dir("temp").unwrap();
+    if Path::new("temp").is_dir() {
+        fs::remove_dir_all("temp").unwrap();
+    }
+    fs::create_dir("temp").unwrap();
 
-    // fs::copy("Input.lua", "temp/temp1.lua").unwrap();
+    fs::copy("Input.lua", "temp/temp1.lua").unwrap();
 
-    // let luac_command = if cfg!(target_os = "windows") {
-    //     "luac"
-    // } else {
-    //     "luac5.1"
-    // };
+    let luac_command = if cfg!(target_os = "windows") {
+        "luac"
+    } else {
+        "luac5.1"
+    };
 
-    // Command::new(luac_command)
-    //     .arg("temp1.lua")
-    //     .current_dir("temp")
-    //     .output()
-    //     .expect("Failed to compile lua binary");
+    Command::new(luac_command)
+        .arg("temp1.lua")
+        .current_dir("temp")
+        .output()
+        .expect("Failed to compile lua binary");
 
     let mut reader = BufReader::new(File::open("temp/luac.out").unwrap());
     let mut buffer = Vec::new();
 
     reader.read_to_end(&mut buffer).unwrap();
 
-    println!("{:?}", buffer);
-
-    let mut memory_stream = MemoryStream::new(buffer);
-
-    println!("{:?}", memory_stream.read_string(6));
+    let mut deserializer = Deserializer::new(buffer);
+    deserializer.deserialize();
 }
