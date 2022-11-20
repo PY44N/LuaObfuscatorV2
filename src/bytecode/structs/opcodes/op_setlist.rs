bbfd80400e61a785b163723fc51917218f80c4ba
PY44N <pyan4444@gmail.com>
2022-11-19 20:51:41 -0500

    I'm running and I'm crying

---
+++ src/bytecode/structs/opcodes/op_setlist.rs
@@ -0,0 +1,36 @@
+use crate::bytecode::{
+    enums::{instruction_type::InstructionType, opcode_type::OpcodeType},
+    structs::{instruction::Instruction, opcode::Opcode},
+};
+
+pub struct OpSetList {
+    pub data: u32,
+    pub opcode: OpcodeType,
+    pub instruction_type: InstructionType,
+    pub data_a: u8,
+    pub data_b: i128,
+    pub data_c: i64,
+}
+
+impl OpSetList {
+    pub fn new(instruction: Instruction) -> Self {
+        Self {
+            data: instruction.data,
+            opcode: instruction.opcode,
+            instruction_type: instruction.instruction_type,
+            data_a: instruction.data_a,
+            data_b: instruction.data_b,
+            data_c: instruction.data_c,
+        }
+    }
+}
+
+impl Opcode for OpSetList {
+    fn get_obfuscated(&self) -> String {
+        todo!()
+    }
+
+    fn is_valid(&self) -> bool {
+        todo!()
+    }
+}
